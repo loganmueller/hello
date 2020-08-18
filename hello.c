a55e5eb7d31626ec78676a2dd57ad83cefc72857
Logan Mueller <lmueller12@huskers.unl.edu>
2020-08-18 01:49:38 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Logan Mueller
+ * Date: 8/17/2020
+ * 
+ * A simple hello world program
+ **/
+ #include <stdlib.h>
+ #include <stdio.h>
+ int main(int argc, char **argv){
+     printf("Hello world!\n");
+     
+     return 0;
+ }
+ 
